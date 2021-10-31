22b3943bd9ca26bbb250c4be2a01bfd7b9a1f191
Marcosa65 <marcosadrian65@gmail.com>
2021-10-31 13:12:12 -0300

    Added godot Sprite
    Added the godot sprite to see the shader effect and other adjustments

--- Point_1.gd
+++ Point_1.gd
@@ -3,4 +3,4 @@ extends RigidBody2D
 func _on_Point_1_input_event(viewport, event, shape_idx):
 	if (event is InputEventMouseButton):
 		if event.is_pressed():
-			apply_central_impulse(Vector2(0,20))
+			apply_central_impulse(Vector2(0,30))
