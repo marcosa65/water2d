a5a4451020236a44763f01bf036532bfc14f185a
Marcosa65 <93166013+marcosa65@users.noreply.github.com>
2022-03-28 16:10:03 -0300

    updated code

--- Point_1.gd
+++ Point_1.gd
@@ -1,6 +1,6 @@
 extends RigidBody2D
 
-func _on_Point_1_input_event(viewport, event, shape_idx):
+func _on_Point_1_input_event(_viewport, event, _shape_idx):
 	if (event is InputEventMouseButton):
 		if event.is_pressed():
 			apply_central_impulse(Vector2(0,30))
